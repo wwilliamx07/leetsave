e687a85c99c37228a13ecef797a6d89b4a479c49
wwilliamx07 <wwilliamx07@gmail.com>
2026-05-07 20:43:30 -0400

    fix

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ async function sendToWebhook(message) {
 function log(out) {
     console.log(out)
     if (!webhook) return
-    sendToWebhook(msg)
+    sendToWebhook(out)
 }
 
 async function solveDaily() {
